40c82a0832cd616dabb33562f1b51e409424710d
JerryHong <s6323859@hotmail.com>
2015-11-01 11:57:36 +0800

    demo 3 complete

---
+++ 3demo/data.js
@@ -0,0 +1,38 @@
+let data = [{
+    "guid": 0,
+    "isActive": false,
+    "price": "$3,453.47",
+    "picture": "http://lorempixel.com/output/cats-h-c-200-300-1.jpg",
+    "name": "Glenda Coffey",
+    "about": "Ipsum exercitation sint dolor id sit commodo veniam sit cillum esse in mollit dolor qui. Proident anim commodo nulla id. Deserunt ipsum nostrud officia dolor esse voluptate nulla voluptate magna dolor et. Est consectetur non nostrud laborum sint voluptate qui et velit.\r\n"
+}, {
+    "guid": 1,
+    "isActive": true,
+    "price": "$2,898.86",
+    "picture": "http://lorempixel.com/output/cats-h-c-200-300-2.jpg",
+    "name": "Melody Strong",
+    "about": "Do nulla veniam minim nostrud non labore cillum Lorem consectetur magna dolore velit velit culpa. Laborum laboris ipsum excepteur in cillum. Deserunt laboris anim nostrud nisi ullamco. Elit et quis tempor sunt aute voluptate non consequat reprehenderit in mollit occaecat. Minim exercitation qui magna aliqua proident officia aliqua quis velit. Exercitation do reprehenderit ex commodo labore officia. Excepteur commodo consequat ex deserunt cillum.\r\n"
+}, {
+    "guid": 2,
+    "isActive": false,
+    "price": "$3,935.09",
+    "picture": "http://lorempixel.com/output/cats-h-c-200-300-3.jpg",
+    "name": "Stephenson Patrick",
+    "about": "Dolor et irure ut labore nulla aliqua excepteur laboris laboris magna amet veniam commodo nulla. Eiusmod deserunt voluptate tempor occaecat. Do cillum aliqua et proident.\r\n"
+}, {
+    "guid": 3,
+    "isActive": false,
+    "price": "$2,272.78",
+    "picture": "http://lorempixel.com/output/cats-h-c-200-300-4.jpg",
+    "name": "Pearl Whitley",
+    "about": "Voluptate minim nulla eu ex elit est qui. Qui Lorem eu sunt ea adipisicing Lorem adipisicing. Dolore eiusmod ea sit duis proident nostrud Lorem ea eiusmod quis eiusmod. Mollit magna ipsum pariatur tempor magna esse laboris anim consectetur aute laboris. Velit anim pariatur in veniam elit. Et nisi voluptate minim velit deserunt ea tempor labore laboris magna. Fugiat irure cillum est ea quis aute sunt et.\r\n"
+}, {
+    "guid": 4,
+    "isActive": true,
+    "price": "$3,951.16",
+    "picture": "http://lorempixel.com/output/cats-h-c-200-300-5.jpg",
+    "name": "Castaneda Cunningham",
+    "about": "Nisi cupidatat tempor id ex elit. Lorem sunt non labore minim sint magna fugiat culpa cillum labore ullamco velit nulla pariatur. Excepteur id nisi et do incididunt esse culpa qui laboris.\r\n"
+}];
+
+export default data;
